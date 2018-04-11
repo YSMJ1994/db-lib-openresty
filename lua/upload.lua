e03b358e61245f65b70088eea6b2cf88a5d847e4
bloss <1248212664@qq.com>
2018-04-11 09:20:22 +0800

    Update 支持文件和图片上传, 并且返回上传后的 url

--- lua/upload.lua
+++ lua/upload.lua
@@ -11,24 +11,36 @@ local md5 = ngx.md5
 local io_open = io.open
 local tonumber = tonumber
 local type = type
-local gsub = string.gsub
 local ngx_var = ngx.var
 local upload_dir = "/Users/wuwaki/playground/digital-static"
 local ngx_req = ngx.req
 local os_time = os.time
 local json_encode = cjson.encode
+local static_prefix = 'static'
 
-
-local function get_ext(res)
-    local ext = 'jpg'
-    if res == 'image/png' then
-        ext = 'png'
-    elseif res == 'image/jpg' or res == 'image/jpeg' then
-        ext = 'jpg'
-    elseif res == 'image/gif' then
-        ext = 'gif'
+-- 根据后缀名获取文件上传路径
+local function get_upload_path(ext)
+    if ext == 'jpg' or ext == 'gif' or ext == 'png' then
+        return '/uploads/images'
+    elseif ext == 'pdf' or ext == 'txt' then
+        return '/uploads/books'
+    else
+        return nil
     end
-    return ext
+end
+
+-- 根据文件名获取文件后缀
+local function get_ext(res)
+    local filename = ngx.re.match(res, '(.+)filename="(.+)"(.*)')
+    if filename then
+        local upload_path
+        local ext = filename[2]:match(".+%.(%w+)$")
+        local upload_path = get_upload_path(ext)
+
+        return ext, upload_path
+    else
+        return nil
+    end
 end
 
 local function in_table(key, table)
@@ -48,14 +60,14 @@ end
 
 local function json_return(code, message, data)
     ngx.header["Content-type"] = "application/json"
-    ngx.say(json_encode({code = code, msg = message, data = {}}))
+    ngx.say(json_encode({code = code, msg = message, data = data}))
 end
 
 local function uploadfile()
     local file
     local file_name
     local form, err = upload:new(chunk_size)
-    local conf = {max_size = 10000000, allow_exts = {'jpg', 'png', 'gif'} }
+    local conf = {max_size = 10000000, allow_exts = {'jpg', 'png', 'gif', 'pdf', 'txt'} }
     local root_path = upload_dir
     local file_info = {extension = '', filesize = 0, url = '', mime = '' }
     local content_len = ngx_req.get_headers()['Content-length']
@@ -73,27 +85,28 @@ local function uploadfile()
             if res[1] == "Content-Type" then
                 file_info.mime = res[2]
             elseif res[1] == "Content-Disposition" then
-
+               
                 local file_id = md5('upload'..os_time())
-                local extension = get_ext(res[2])
-                file_info.extension = extension
+                local ext, upload_path = get_ext(res[2])
 
-                if not extension then
+                if not ext or not upload_path then
                     return nil,  '未获取文件后缀'
                 end
 
-                if not in_table(extension, conf.allow_exts) then
+                file_info.extension = ext
+
+                if not in_table(ext, conf.allow_exts) then
                     return nil,  '不支持该文件格式'
                 end
 
-                local dir = root_path..'/uploads/images/'
+                local dir = root_path..upload_path..'/'
                 if file_exists(dir) ~= true then
                     local status = os_exec('mkdir -p '..dir)
                     if status ~= true then
                         return nil, '创建目录失败'
                     end
                 end
-                file_name = dir..file_id.."."..extension
+                file_name = dir..file_id.."."..ext
                 if file_name then
                     file = io_open(file_name, "w+")
                     if not file then
@@ -111,8 +124,8 @@ local function uploadfile()
                 file = nil
             end
         elseif typ == "eof" then
-            file_name = gsub(file_name, root_path, '')
-            file_info.url = file_name
+            file_name = ngx.re.sub(file_name, root_path, '')
+            file_info.url = static_prefix..file_name
             return file_info
         else
 
@@ -123,7 +136,7 @@ end
 
 local file_info, err = uploadfile()
 if file_info then
-    json_return(200, '上传成功', {imgurl = file_info.url})
+    json_return(200, '上传成功', { imgurl = file_info.url })
 else
-    json_return(5003, err)
+    json_return(5003, err, nil)
 end
